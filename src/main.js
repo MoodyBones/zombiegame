4e40b536e04a6729e99dcc0f13bf811ab599ddf9
Mel Jones <mel.jones@hyretalents.com>
2021-08-30 15:58:53 +0200

    intial project and store setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
-import store from "./store";
-
+import store from "./store"; // add store manually here
 Vue.config.productionTip = false;
 
 new Vue({
-  store,
+  store, // add store using destructuring
   render: (h) => h(App),
 }).$mount("#app");
